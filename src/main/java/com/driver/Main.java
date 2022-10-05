6e5e5f00a0ec92971f185a12a5cb75ed76042c44
amitjait <amit.rimit591@gmail.com>
2022-10-05 11:58:10 +0530

    updating

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+    /*  obj.name = "Amit"; will get an error ands need to implement setter function to set the name and for getting the name 
+     need to implement getter() */
+    obj.setter("AccioJob");
+    System.out.println(obj.getter());
+
+  }
 }
